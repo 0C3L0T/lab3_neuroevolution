48e41380040d58fa9ea0a5600c5d4c32bb70922e
Merijn <merijn.laks@student.uva.nl>
2025-10-02 21:34:55 +0200

    feat: init and prototype functionality

--- individual.py
+++ individual.py
@@ -1,12 +1,16 @@
 
 ## Standard library
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, Any, List
+from typing import TYPE_CHECKING, Any, Callable, List
 
 ## Third party libraries
+from ariel.body_phenotypes.robogen_lite.constructor import construct_mjspec_from_graph
+from ariel.body_phenotypes.robogen_lite.modules.core import CoreModule
+from ariel.utils.tracker import Tracker
 import mujoco as mj
 import numpy as np
 import torch
+from evotorch import Problem
 
 ## Local libraries
 from ariel.simulation.controllers.controller import Controller
@@ -15,6 +19,8 @@ from ariel.body_phenotypes.robogen_lite.decoders.hi_prob_decoding import (
     HighProbabilityDecoder,
     save_graph_as_json,
 )
+
+from simulation import run_simulation
 
 # Type Checking
 if TYPE_CHECKING:
@@ -44,29 +50,36 @@ TODO
 - can we use the built-in Individual class?
 - write constructor, or keep imperative? 
 '''
-@dataclass
 class Individual:
     id: int
     genome: Genome
     body_graph: DiGraph[Any]
-    brain: torch.Tensor
+    network: torch.nn
+    weights: torch.Tensor
     fitness: Fitness
+    controller_callback: Callable
+
+    def __init__(
+            self,
+            nde: NeuralDevelopmentalEncoding,
+            hpd: HighProbabilityDecoder,
+            id: int,
+            ):
+        self.id = id
+        self.genome = create_genome()
+        self.body_graph = create_body_graph(nde, hpd, self.genome)
+
+        # figure out model input/ouput
+            # is there a way to get these without initialising mujoco?
+        
+        # init NN
+
+        # init weights
 
 
-def initialize_individual_brain(individual: Individual) -> None:
-    return None
 
-def create_individual_body(
-        nde: NeuralDevelopmentalEncoding,
-        hpd: HighProbabilityDecoder
-        ) -> DiGraph[Any]:
-    """
-    Create a random robot graph using the NDE.
-    NOTE: A graph is not yet a body
 
-    TODO
-    - store the layout of the body somewhere so we can init the brains later
-    """
+def create_genome(void) -> Genome:
     type_p_genes = RNG.random(GENOTYPE_SIZE).astype(np.float32)
     conn_p_genes = RNG.random(GENOTYPE_SIZE).astype(np.float32)
     rot_p_genes = RNG.random(GENOTYPE_SIZE).astype(np.float32)
@@ -77,7 +90,18 @@ def create_individual_body(
         rot_p_genes,
     ]
 
-    p_matrices = nde.forward(genotype)
+    return genotype
+
+
+def create_body_graph(
+        nde: NeuralDevelopmentalEncoding,
+        hpd: HighProbabilityDecoder,
+        genome: Genome
+        ) -> DiGraph[Any]:
+    """
+    NOTE: A graph is not yet a body
+    """
+    p_matrices = nde.forward(genome)
 
     # Decode the high-probability graph
     robot_graph: DiGraph[Any] = hpd.probability_matrices_to_graph(
@@ -88,8 +112,6 @@ def create_individual_body(
 
     return robot_graph
 
-def create_individual(nde: NeuralDevelopmentalEncoding, hpd: HighProbabilityDecoder) -> Individual:
-    pass
 
 def evaluate_individual(individual: Individual) -> Fitness:
     '''
@@ -102,6 +124,23 @@ def evaluate_individual(individual: Individual) -> Fitness:
         - run simulation
         - evaluate fitness (distance)
     '''
+    core: CoreModule = construct_mjspec_from_graph(individual.body_graph)
+    
+    mujoco_type_to_find = mj.mjtObj.mjOBJ_GEOM
+    name_to_bind = "core"
+
+    # not sure what this does
+    tracker = Tracker(
+        mujoco_obj_to_find=mujoco_type_to_find,
+        name_to_bind=name_to_bind,
+    )
+    
+    ctrl = Controller(
+        controller_callback_function=individual.controller_callback,
+        tracker=tracker,
+    )
+
+    run_simulation(individual, ctrl, core)
 
     return 0.0
 
@@ -110,6 +149,10 @@ def train_individual(individual: Individual) -> None:
     this is where we use evotorch Problem and CMA-ES
     assuming here that the brain is a Tensor
     '''
+    problem = Problem(
+        "max",
+
+    )
 
     return None
 
