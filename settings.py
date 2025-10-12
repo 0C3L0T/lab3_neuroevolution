b2e32aa06bd3cb93d718759770afd078b09ed488
Merijn <merijn.laks@student.uva.nl>
2025-10-12 21:44:44 +0200

    update settings for AWS

--- settings.py
+++ settings.py
@@ -2,23 +2,23 @@
 import os
 
 # the number of parts in a body
-NUM_BODY_MODULES = 30
+NUM_BODY_MODULES = 10
 
 # how many bodies to train at a time
 NUM_BODY_ACTORS = 2
 
 # how many brains to train at the same time
-NUM_BRAIN_ACTORS = os.cpu_count() // NUM_BODY_ACTORS
+NUM_BRAIN_ACTORS = 20
 
 # how many brains to use in brain population
-BRAIN_POPULATION_SIZE = 48
+BRAIN_POPULATION_SIZE = 30
 
 # arena size in tournament selection
 ARENA_SIZE = 5
 
 # how many bodies to use in a population, should be multiple of 6
-BODY_POPULATION_SIZE = 24
+BODY_POPULATION_SIZE = 18
 
 # how many iterations to train body for
 # brains have dynamic training iterations
-DEFAULT_BODY_ITERATIONS = 100
+DEFAULT_BODY_ITERATIONS = 9999
